eddb62aa4da127233ce8386fbcc6dc91ecc52cbd
Isaias  Siqueira <99566627+siqueira91@users.noreply.github.com>
2023-07-04 05:13:28 -0300

    Update ContaTerminal.java

---
+++ Desafio01/Controle de Fluxo/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero; 
+        String agencia; 
+        String nomeCliente; 
+        double saldo;
+
+        System.out.println("Por favor, digite o número da Conta!");
+        numero = sc.nextInt();
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = sc.next();
+        System.out.println("Por favor, digite o nome do Cliente!");
+        nomeCliente = sc.next();
+        System.out.println("Por favor, digite o Saldo da Conta!");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+
+        sc.close();
+
+    }
+
+}
